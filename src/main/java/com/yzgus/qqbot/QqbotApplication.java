415a1e4fb7c6ccedb54a3630b6aaeca87c5e74ef
YZGUS <953433612@qq.com>
2025-03-15 10:59:47 +0800

    初始化项目

---
+++ src/main/java/com/yzgus/qqbot/QqbotApplication.java
@@ -0,0 +1,12 @@
+package com.yzgus.qqbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqbotApplication {
+
+	public static void main(String[] args) {
+	  	SpringApplication.run(QqbotApplication.class, args);
+	}
+}
